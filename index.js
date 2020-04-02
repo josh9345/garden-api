ce742590f9d45021089fec1d9b95f2bccc6525fc
Joshua Reed <joshualreed93@gmail.com>
2020-04-02 10:59:01 -0400

    added cors

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express = require("express");
 const app = express();
 const parser = require("body-parser");
 const routes = require("./routes/index");
+const cors = require("cors");
 
+app.use(cors());
 app.use(parser.json());
 app.use("/", routes);
 
